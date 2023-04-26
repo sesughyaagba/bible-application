8c39e1265885a9b101aa644d2e7f5867b6919a60
sesughyaagba <sesughdesmond@gmail.com>
2023-04-26 17:32:59 +0100

    little update

--- javascript/bibleApp2.js
+++ javascript/bibleApp2.js
@@ -27,30 +27,31 @@ function function1(script, data) {
     var li = document.createElement("li");
     li.textContent = script[i];
     var div2 = document.createElement("div");
+    var chap = document.createElement("div");
+    chap.classList.add("chapter");
 
     // add event listener 'click' to each 'li' element
 
     li.addEventListener("click", () => {
       let chapters = Object.keys(data[script[i]]);
       console.log(script[i]);
-      let bible = document.querySelector(".bible");
 
       // new addition
       h1.textContent = script[i];
 
-      // stop
+      div2.innerHTML = "";
+      chap.textContent = "";
+
       for (let j = 0; j < chapters.length; j++) {
         let h4 = document.createElement("h4");
         h4.textContent = `Chapter ${chapters[j]}`;
+        h4.addEventListener("click", () => {
+          chap.innerHTML = data[script[i]][chapters[j]];
+        });
         div2.appendChild(h4);
-
-        // <div>${data[script[i]][chapters[j]]}</div>;
-
-        // Append 'div' to 'body'
       }
-      var chap = document.createElement("div");
-      chap.classList.add("chapter");
-      //   div.replaceChild(chap);
+      div.appendChild(div2);
+      div.appendChild(chap);
     });
 
     // Append 'li' to 'ul'
@@ -59,7 +60,6 @@ function function1(script, data) {
   }
 
   body.append(h1);
-  div.replaceChildren(div2);
 
   body.append(div);
 }
@@ -84,26 +84,46 @@ function function2(script, data) {
   var body = document.querySelector(".new_test_body");
   console.log(ul);
 
+  let h1 = document.createElement("h1");
+  var div = document.createElement("div");
+  div.classList.add("bible");
+
   // loops through the 'script' array and creates a list 'li' for each one
   for (let i = 0; i < script.length; i++) {
     var li = document.createElement("li");
     li.textContent = script[i];
+    var div2 = document.createElement("div");
+    var chap = document.createElement("div");
+    chap.classList.add("chapter");
 
     // add event listener 'click' to each 'li' element
     li.addEventListener("click", () => {
       let chapters = Object.keys(data[script[i]]);
-      for (let j = 0; j < chapters.length; j++) {
-        var div = document.createElement("div");
-        div.innerHTML = `<h1>Chapter ${chapters[j]}</h1><div>${
-          data[script[i]][chapters[j]]
-        }</div>`;
+      console.log(script[i]);
+      // new addition
+      h1.textContent = script[i];
 
-        // Append 'div' to 'body'
-        body.appendChild(div);
+      div2.innerHTML = "";
+      chap.textContent = "";
+
+      for (let j = 0; j < chapters.length; j++) {
+        let h4 = document.createElement("h4");
+        h4.textContent = `Chapter ${chapters[j]}`;
+        h4.addEventListener("click", () => {
+          chap.innerHTML = data[script[i]][chapters[j]];
+        });
+        div2.appendChild(h4);
       }
+      div.appendChild(div2);
+      div.appendChild(chap);
     });
 
     // Append 'li' to 'ul'
     ul.appendChild(li);
+    console.log(JSON.stringify(div2));
   }
+
+  body.append(h1);
+
+  body.append(div);
 }
